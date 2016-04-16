ec38885dd7b17e925740bfed703273e7417e62f2
Derek Barncard <derek.barncard@hellosign.com>
2016-04-16 01:08:12 -0700

    Testing additions

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # hellosign-embedded
 A JavaScript library required for embedding HelloSign features into your webapp. For more information, see our API documentation at https://www.hellosign.com/api/documentation.
 
-## Building
+NOTE: DOCUMENTATION IS CURRENTLY IN PROGRESS (04/15/2016). Read at your own peril.
+
+# Getting started
+
+You can load this library from our global CDN at:
+````
+coming soon
+````
+and include in your page in a script tag.
+
+Alternately see below to:
+* Build your own version from source
+* Bundle with your front-end using Webpack or browserify
+
+
+## Building from source
+You'll need to clone this repository, and have npm and webpack installed.
+From your command line, run `webpack`, which will build both the minified and the unminified versions of the script for you under the `dist` directory.
+
 
 ## Webpack
 You can require the hellosign-embedded package in your front-end code when using a bundler like browserify or webpack.
@@ -17,3 +35,5 @@ If transpiling for ES6, you can also use the `import` statement:
 import HelloSign from 'hellosign-embedded';
 window.HelloSign = HelloSign;
 ````
+
+#Usage
--- tests/module.js
+++ tests/module.js
@@ -1,20 +1,47 @@
+// Test Env
 var expect = require('chai').expect;
-require('jsdom-global')()
-var HelloSign = require('../src/embedded.js');
+var jsdomGlobal = require('jsdom-global')();
+
+var HelloSign;
+var clientId = "IAMA_FAKE_CLIENTID_AMA";
+var fakeUrl = "http://api.hellosign.com/fakepath/87687fds";
 
 describe('Module basics', function(){
+
+    beforeEach(function(){
+        HelloSign = require('../src/embedded.js');
+    });
 
     it('should be defined', function(){
         expect(HelloSign.open).to.not.equal(undefined);
     });
 
-    it('should have an open function', function(){
+    it('should have base functions defined', function(){
+        expect(HelloSign.init).to.be.a('function');
         expect(HelloSign.open).to.be.a('function');
+        expect(HelloSign.ensureParentDomain).to.be.a('function');
     });
 
 });
 
+describe('HelloSign.init', function(){
+    beforeEach(function(){
+        HelloSign = require('../src/embedded.js');
+    });
+
+    it('should accept and set a client ID', function(){
+        HelloSign.init(clientId);
+        expect(HelloSign.clientId).to.equal(clientId);
+    });
+});
+
 describe('HelloSign.open', function(){
+    beforeEach(function(){
+        HelloSign = require('../src/embedded.js');
+    });
+    afterEach(function(){
+        HelloSign = undefined;
+    });
 
     it('should require args', function(){
         var errorMatch;
@@ -37,10 +64,22 @@ describe('HelloSign.open', function(){
     it('should accept an arguments hash', function(){
         var thrown = false;
         try {
-            HelloSign.open({url: "hodor"});
+            HelloSign.open({url: fakeUrl});
         } catch (e) {
             thrown = true;
         }
         expect(thrown).to.equal(false);
+    });
+
+    it('should open an iframe', function(){
+        HelloSign.init(clientId);
+        HelloSign.open({url: fakeUrl});
+        var iframes = document.getElementsByTagName('iframe');
+        expect(Object.keys(iframes).length).to.not.equal(0);
+        expect(HelloSign.iframe).to.equal(iframes['0']);
+    });
+
+    after(function(){
+        jsdomGlobal(); // Reset global env
     });
 });
