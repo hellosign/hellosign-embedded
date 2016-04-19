056d28fe540057ca89df8dda6b7440c72fcb2851
Derek Barncard <derek.barncard@hellosign.com>
2016-04-19 15:23:50 -0700

    Updated travis settings

---
+++ pre_deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+currentVersion=`git describe --tags`
+distDir="dist"
+fnCurrentFull="hellosign-embedded.${currentVersion}.js"
+fnCurrentMin="hellosign-embedded.${currentVersion}.min.js"
+fnLatestFull="hellosign-embedded.LATEST.js"
+fnLatestMin="hellosign-embedded.LATEST.min.js"
+
+cp ${distDir}/${fnCurrentFull} ${distDir}/${fnLatestFull}
+cp ${distDir}/${fnCurrentMin} ${distDir}/${fnLatestMin}
+
+exit 0
