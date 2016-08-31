4611fde746bd36e7e7ded8613a5c8ba550fe1c09
Chris Paul <chris@hellosign.com>
2016-08-31 16:55:10 -0700

    Backport embedded.js to legacy CDN location.

--- scripts/pre_deploy.sh
+++ scripts/pre_deploy.sh
@@ -6,8 +6,10 @@ fnCurrentFull="hellosign-embedded.${currentVersion}.js"
 fnCurrentMin="hellosign-embedded.${currentVersion}.min.js"
 fnLatestFull="hellosign-embedded.LATEST.js"
 fnLatestMin="hellosign-embedded.LATEST.min.js"
+fnOldName="embedded.js"
 
 cp ${distDir}/${fnCurrentFull} ${distDir}/${fnLatestFull}
 cp ${distDir}/${fnCurrentMin} ${distDir}/${fnLatestMin}
+cp ${distDir}/${fnCurrentMin} ${distDir}/${fnOldName}
 
 exit 0
