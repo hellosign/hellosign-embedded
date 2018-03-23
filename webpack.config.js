4c210d5bbe4ae3d0ed0552fabf3a6753ffa3b88a
Nathan Buchar <nathan.buchar@hellosign.com>
2018-03-23 10:29:27 -0700

    [DEV-3932] Implement Babel to allow transpiling for browser support
    This should have been a thing before

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,74 @@
-var version = require('./package.json').version;
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const path = require('path');
 
-module.exports =
-[
-    {
-        name: "Minified",
-        entry: "./src/embedded.js",
-        output: {
-            path: __dirname + "/dist",
-            filename: "hellosign-embedded." + version + ".min.js",
-            library: "HelloSign",
-            libraryTarget: "var"
-        },
-        module: {
-            loaders: [
-                {
-                    test: /\.js/,
-                    loader: 'uglify',
-                    include: __dirname + "/src"
-                },
-                {
-                    test: /\.json/,
-                    loader: 'json',
-                    include: __dirname
-                }
-            ]
-        }
+const { version } = require('./package.json');
+
+module.exports = [
+  {
+    entry: './src/embedded.js',
+    mode: 'development',
+    devtool: 'inline-source-map',
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: `hellosign-embedded.${version}.js`,
+      library: 'HelloSign',
+      libraryTarget: 'var'
     },
-    {
-        name: "Uncompressed",
-        entry: "./src/embedded.js",
-        output: {
-            path: __dirname + "/dist",
-            filename: "hellosign-embedded." + version + ".js",
-            library: "HelloSign",
-            libraryTarget: "var"
-        },
-        module: {
-            loaders: [
-                {
-                    test: /\.json/,
-                    loader: 'json',
-                    include: __dirname
-                }
-            ]
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                  require('@babel/plugin-proposal-object-rest-spread'),
+                  require('@babel/plugin-transform-object-assign')
+                ]
+              }
+            }
+          ]
         }
+      ]
     }
+  },
+  {
+    entry: './src/embedded.js',
+    mode: 'production',
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: `hellosign-embedded.${version}.min.js`,
+      library: 'HelloSign',
+      libraryTarget: 'var'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                  require('@babel/plugin-proposal-object-rest-spread'),
+                  require('@babel/plugin-transform-object-assign')
+                ]
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new UglifyJsPlugin({
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/
+      })
+    ]
+  }
 ];
