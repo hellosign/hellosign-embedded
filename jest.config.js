337fb28bdfdf9d618aaa0f307736e65022e3319c
Nathan Buchar <nathan.buchar@hellosign.com>
2018-08-29 16:46:28 -0700

    Add eslint runner to jest tests

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,17 @@
 const globals = require('./globals');
 
-// module.exports = {
-//   projects: [
-//     {
-//       runner: 'jest-runner-eslint',
-//       displayName: 'eslint',
-//       testMatch: ['<rootDir>/src/**/*.js', '<rootDir>/index.js'],
-//     },
-//     {
-//       collectCoverage: true,
-//       coverageDirectory: '.coverage',
-//       globals,
-//     },
-//   ],
-// };
-
 module.exports = {
-  collectCoverage: true,
-  coverageDirectory: '.coverage',
-  testURL: 'http://localhost/',
-  globals,
+  projects: [
+    {
+      runner: 'jest-runner-eslint',
+      displayName: 'eslint',
+      testMatch: ['<rootDir>/src/**/*.js', '<rootDir>/index.js'],
+    },
+    {
+      collectCoverage: true,
+      coverageDirectory: '.coverage',
+      testURL: 'http://localhost/',
+      globals,
+    },
+  ],
 };
