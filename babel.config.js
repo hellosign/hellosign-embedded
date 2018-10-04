962103a84070bc85c607cb8c380faa1299aecf04
Nathan Buchar <nathan.buchar@hellosign.com>
2018-10-04 10:46:03 -0700

    chore: update dependencies

--- babel.config.js
+++ babel.config.js
@@ -9,6 +9,5 @@ module.exports = {
   plugins: [
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-transform-object-assign',
   ],
 };
