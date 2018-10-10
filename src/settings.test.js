67528804dd916e264f9ead50fe995a4402741fbf
Nathan Buchar <1815367+nathanbuchar@users.noreply.github.com>
2018-10-10 11:48:59 -0700

    [DEV-4322] HelloSign Embedded v2 (#77)
    refactor(DEV-4322): HelloSign Embedded v2
    
    BREAKING CHANGE: `HelloSign` is now a class, not a singleton.
    BREAKING CHANGE: `init()` has been deprecated
    BREAKING CHANGE: The `url` option is now the first argument of `open()`
    BREAKING CHANGE: `open()` options are now defined as the second argument
    BREAKING CHANGE: `userCulture` option is now named `locale`
    BREAKING CHANGE: `healthCheckoutTimeoutMs` option is now named `timeout`
    BREAKING CHANGE: `requester` option is now named `requestingEmail`
    BREAKING CHANGE: `messageListener` option has been removed
    BREAKING CHANGE: `uxVersion` option has been removed
    BREAKING CHANGE: `height` option has been removed
    BREAKING CHANGE: `HelloSign.CULTURES` is now `HelloSign.locales`
    BREAKING CHANGE: `HelloSign.VERSION` is now `HelloSign.version`
    
    Fixes #27, fixes #31, fixes #36, fixes #40, fixes #47, fixes #57, fixes #75.

---
+++ src/settings.test.js
@@ -0,0 +1,28 @@
+import settings from './settings';
+
+describe('settings', () => {
+
+  test('exports classNames', () => {
+    expect(settings.classNames).toBeDefined();
+  });
+
+  test('exports events', () => {
+    expect(settings.events).toBeDefined();
+  });
+
+  test('exports iframe', () => {
+    expect(settings.iframe).toBeDefined();
+  });
+
+  test('exports locales', () => {
+    expect(settings.locales).toBeDefined();
+  });
+
+  test('exports messages', () => {
+    expect(settings.messages).toBeDefined();
+  });
+
+  test('exports types', () => {
+    expect(settings.types).toBeDefined();
+  });
+});
